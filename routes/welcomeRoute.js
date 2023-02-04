0c9d2d5cd209d2c23ccbba98d44c9fd0d0ff801e
Deepak Tiwari <deepak.tiwari@nagarro.com>
2023-02-04 15:30:00 +0530

    webhook setp done

--- routes/welcomeRoute.js
+++ routes/welcomeRoute.js
@@ -1,8 +1,28 @@
 import express from 'express';
+import {WebhookClient,Card, Suggestion} from 'dialogflow-fulfillment';
 
 const router = express.Router();
 
-router.get("/", async (req, res) => {
+function welcome(agent) {
+  agent.add(`Welcome to my api agent!`);
+}
+
+function fallback(agent) {
+  agent.add(`I didn't understand`);
+  agent.add(`I'm sorry, can you try again?`);
+}
+
+router.get("/", async (request, response) => {
+  const agent = new WebhookClient({request, response});
+  console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
+  console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
+
+  let intentMap = new Map();
+  intentMap.set('Default Welcome Intent', welcome);
+  intentMap.set('Default Fallback Intent', fallback);
+  // intentMap.set('your intent name here', yourFunctionHandler);
+  // intentMap.set('your intent name here', googleAssistantHandler);
+  agent.handleRequest(intentMap);
   res.send('Welcome to node js');
 });
 
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 import express from 'express';
+import {WebhookClient,Card, Suggestion} from 'dialogflow-fulfillment';
 import mongoose from 'mongoose';
 import config from './config';
-import welcomeRoute from './routes/welcomeRoute';
+//import welcomeRoute from './routes/welcomeRoute';
 
 const mongodbUrl = config.MONGODB_URL;
 mongoose
@@ -15,8 +16,23 @@ mongoose
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use('/api/welcome', welcomeRoute);
+app.get('/', (req, res) => {
+  res.send("Server Is Working......")
+})
+app.post('/webhook', (req, res) => {
+  // get agent from request
+  let agent = new WebhookClient({request: req, response: res})
+  // create intentMap for handle intent
+  let intentMap = new Map();
+  // add intent map 2nd parameter pass function
+  intentMap.set('Send Course Detail to User',handleSendCourseDetailToUser)
+  // now agent is handle request and pass intent map
+  agent.handleRequest(intentMap)
+})
+function handleSendCourseDetailToUser(agent){
+  agent.add("Hello I am Webhook demo your course details sent your mail id.")
+}
 
 app.listen(config.PORT, () => {
-  console.log('Server started at http://localhost:5001');
+  console.log('Server started at http://localhost:'+config.PORT);
 });
